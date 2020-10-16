00845340d65fb7e72d66319740cd4323ec59910a
Alexander Kanonirov <youngsavagesohard@gmail.com>
2020-10-16 19:22:06 +0300

    CreateSTL

--- CreateSTL.py
+++ CreateSTL.py
@@ -3,16 +3,16 @@ import Create
 
 if not Create.check_argc(len(sys.argv)):sys.exit(1)
 Zco = []
-if not Create.ReadFile(str(sys.argv[1]),Zco):sys.exit(1)
+if not Create.read_file(str(sys.argv[1]),Zco):sys.exit(1)
 if len(Zco) < 2:
     print("Not enough data!")
     sys.exit(1)
 print("Creating " + str(sys.argv[2]))
 print("  1.Creating Triangles")
 Triangles = []
-Create.CreateTriangles(Zco,Triangles)
+Create.create_triangles(Zco,Triangles)
 print("  2.Creating Output File")
 Outputstr = []
-Create.CreateOutput(Triangles,Create.CalculateCoeff(Zco),Outputstr)
-Create.CreateSTL(str(sys.argv[2]),Outputstr)
+Create.create_output(Triangles,Create.calculate_coeff(Zco),Outputstr)
+Create.create_STL(str(sys.argv[2]),Outputstr)
 print("Done!")
